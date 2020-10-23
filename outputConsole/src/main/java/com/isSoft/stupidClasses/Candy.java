5554325c6ab4de0a591421ab401fd9a915b5f5d3
Marina <marinly375@gmail.com>
2020-10-24 01:51:27 +0300

    Add project multiModule

---
+++ outputConsole/src/main/java/com/isSoft/stupidClasses/Candy.java
@@ -0,0 +1,50 @@
+package com.isSoft.stupidClasses;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.UUID;
+
+public class Candy {
+    private final static Logger log = LoggerFactory.getLogger(Candy.class);
+    private final String identificationNumber;
+    private final String nameCandy;
+    private  double weight;
+    private Candy (String identificationNumber, String nameCandy, double weight){
+        log.info("Creating candy {}", identificationNumber);
+        this.identificationNumber =  identificationNumber;
+        this.nameCandy = nameCandy;
+        this.weight = weight;
+        log.info("Created candy {}", identificationNumber);
+    }
+    public static Candy createCandy(String nameCandy, double weight){
+        return new Candy(UUID.randomUUID().toString(), nameCandy, weight);
+    }
+
+    public String getIdentificationNumber() {
+        return identificationNumber;
+    }
+
+    public String getNameCandy() {
+        return nameCandy;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+    public void setWeight(double weight) throws IllegalArgumentException{
+        if(weight > 0.0)
+            this.weight = weight;
+        else
+            throw  new IllegalArgumentException("The weight must be greater than 0.0");
+    }
+
+
+    @Override
+    public String toString() {
+        return "Candy{ " +
+                "identificationNumber='" + identificationNumber + '\'' +
+                ", nameCandy='" + nameCandy + '\'' +
+                ", weight=" + weight +
+                '}';
+    }
+}
---
+++ outputToFile/src/main/java/com/isSoft/stupidClases/Main.java
@@ -0,0 +1,21 @@
+package com.isSoft.stupidClases;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+    public static void main(String[] args) {
+        log.debug("Start of the program");
+        Candy candy1 = Candy.createCandy("Барбариска", 0.12);
+        Candy candy2 = Candy.createCandy("Ирис", 0.13);
+        try {
+            candy1.setWeight(0);
+        }
+        catch (IllegalArgumentException e){
+            log.error("Incorrect weight! ", e);
+        }
+        log.debug("The end of the program");
+
+    }
+}
